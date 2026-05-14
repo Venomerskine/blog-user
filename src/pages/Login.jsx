f5dfcb643cf3993bade26fb52a62998c5487141b
Lincoln Kubasu <kubasulincoln@gmail.com>
2026-05-14 09:05:05 +0300

    Login succesfull logic

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,18 +1,49 @@
 import { Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 
 
 function Login () {
-  const handleSignup = async (event) => {
+  const navigate = useNavigate();
+
+
+  const handleLogin = async (event) => {
     event.preventDefault();
 
-  }
+    const formData = new FormData(event.currentTarget);
+    const data = Object.fromEntries(formData);
+
+    try {
+      const response = await fetch('http://localhost:3000/api/users/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+      });
+
+      const result = await response.json();
+
+      if (response.ok) {
+        localStorage.setItem('token', result.token);
+        navigate('/dashboard');
+      } else {
+        alert("Invalid Credentials");
+      }
+
+    } catch (error) {
+      console.log("Login Error!", error);
+    }
+  };
 
   return (
     <>
-      <form>
-      <h1>Log to your account</h1>
+      <form onSubmit={handleLogin}>
+      <h1>Log In</h1>
         <label htmlFor="email">Email: </label>
-        <input type='email' id="email"></input>
+        <input type='email' id="email" name="email"></input>
+
+        <label htmlFor="password">Password</label>
+        <input id="password" name="password"></input>
+
+        <button type="submit">Login</button>
       </form>
       <Link to="/signup">
       <button>Switch to sign up</button>

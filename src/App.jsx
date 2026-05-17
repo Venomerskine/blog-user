a7a5051dbd60bc2383331aa0bc757d7d0ef1d794
Lincoln Kubasu <kubasulincoln@gmail.com>
2026-05-17 06:30:51 +0300

    worjed on the token verifiaction, fetching dashboard form api

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Login from './pages/Login'
 import Signup from './pages/Signup'
 import Dashboard from './pages/Dashboard'
 import { Route, Routes } from 'react-router-dom'
+import ProtectedRoute from './components/protectedRoute'
 
 
 
@@ -19,7 +20,14 @@ function App() {
 
     <Route path='/login' element={<Login/>}/>
     <Route path='/signup' element={<Signup/>}/>
-    <Route path='/dashboard' element={<Dashboard/>} />
+
+    <Route
+     path='/dashboard' 
+     element={
+      <ProtectedRoute>
+     <Dashboard/>
+      </ProtectedRoute>
+     } />
 
     </Routes>
   )
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,81 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 function Dashboard() {
+    const [isAuth, setIsAuth] = useState(null);
+    const navigate = useNavigate()
+
+    useEffect (() => {
+        const verifyUSer = async () => {
+            const token = localStorage.getItem("token");
+
+            if (!token) {
+                setIsAuth(false)
+                navigate("/login");
+                return;
+            }
+
+            try {
+                const res = await fetch("http://localhost:3000/api/users/auth/verify", {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                    },
+                });
+
+                if(!res.ok) {
+                    localStorage.removeItem("token");
+                    setIsAuth(false)
+                    navigate("/login");
+                } else {
+                    setIsAuth(true)
+                }
+            } catch (err) {
+                setIsAuth(false)
+                navigate("/login")
+                console.log("Server Down!", err)
+            }
+        }
+
+        verifyUSer();
+
+    }, [])
+
+    const getAllPosts = async () => {
+
+        try {
+            const token = localStorage.getItem("token");
+
+            if (!token) {
+                console.log("No token")
+                return;
+            }
+    
+            const response = await fetch('http://localhost:3000/api/posts', {
+                method: 'GET',
+                headers: { 
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${token}`,
+                 },
+            })
+
+            const data = await response.json()
+
+            console.log("Posts in dashboard get : ", data)
+
+        } catch (err) {
+            console.log("Error fetching posts: ", err)
+        }
+
+    }
+
+
+
+    useEffect(() => {
+        getAllPosts
+    }, []);
+
+    if (isAuth === null ) return <p>Cheching auth...</p>
+
     return(
         <>
             <h1>Dashboard</h1>
